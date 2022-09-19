6c9a8b1cdd45cff426ccf50f6fbd86984449e788
Cellou <72306494+CellouDL@users.noreply.github.com>
2022-09-19 17:34:01 +0100

    bugs fixed. Now work

--- Dealabs.Price.Error/Program.cs
+++ Dealabs.Price.Error/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.IO;
+
 using System.Reflection.Emit;
 using System.Reflection.PortableExecutable;
 using System.Xml;
@@ -10,8 +11,8 @@ using System.Xml.Linq;
 using System.Data.SqlTypes;
 using static System.Net.Mime.MediaTypeNames;
 using static System.Net.WebRequestMethods;
-
-
+using System.Security.Cryptography;
+using File = System.IO.File;
 
 var targetUrl = new Uri("https://www.dealabs.com/groupe/erreur-de-prix");
 var webReq = (HttpWebRequest)WebRequest.Create(targetUrl);
@@ -22,128 +23,57 @@ bool FileCompare(string file1, string file2)
 {
     int file1byte;
     int file2byte;
-    FileStream fs1;
-    FileStream fs2;
-
-    fs1 = new FileStream(file1, FileMode.Open);
-    fs2 = new FileStream(file2, FileMode.Open);
-    
+    using var fs1 = File.OpenRead(file1);
+    using var fs2 = File.OpenRead(file2);
     if (fs1.Length != fs2.Length)
-    {
-        fs1.Close();
-        fs2.Close();
-
         return false;
-    }
+    using var br1 = new BinaryReader(fs1);
+    using var br2 = new BinaryReader(fs2);
 
-    do
+    while (fs1.Position < fs1.Length)
     {
-        file1byte = fs1.ReadByte();
-        file2byte = fs2.ReadByte();
-        Console.WriteLine(file1byte + "\n" + file2byte);
+        var b1 = br1.ReadByte();
+        var b2 = br2.ReadByte();
+
+        if (b1 != b2)
+            return false;
     }
-    while ((file1byte == file2byte) && (file1byte != -1));
 
-    fs1.Close();
-    fs2.Close();
-
-    return ((file1byte - file2byte) == 0);
+    return true;
 }
 
-
-
-
-//Récupère le code source de la page erreur de prix de dealabs et les écrit en html
 using (WebResponse webRes = webReq.GetResponse())
 {
     Stream stream = webRes.GetResponseStream();
     StreamReader readStream = new StreamReader(stream, Encoding.UTF8);
     var texte = readStream.ReadToEnd();
-    StreamWriter sourceCode1 = new StreamWriter("dealabs.html", false);
-    sourceCode1.Write(texte);
+
+    texte = texte.Substring(texte.IndexOf("class=\"cept-tt thread-link linkPlain thread-title--list js-thread-title\"") + "class=\"cept-tt thread-link linkPlain thread-title--list js-thread-title\"".Length);
+    texte = texte.Substring(texte.IndexOf(")\"") + ")\"".Length);
+    string decoupe = texte.Substring(0, texte.IndexOf("</strong"));
+    decoupe = decoupe.Substring(texte.IndexOf("data-t-click") + "data-t-click".Length);
+    string final = decoupe.Substring(1, decoupe.IndexOf(" - "));
+    Console.WriteLine(final);
+    StreamWriter sourceCode1 = new StreamWriter("dealabs.txt", false);
+    sourceCode1.Write(final);
     sourceCode1.Close();
     webRes.Close();
     readStream.Close();
 
-
-    if (FileCompare("dealabs.html", "dealabs1.html"))
-    {
-        Console.WriteLine("Files are equal.");
-    }
+    if (FileCompare("dealabs.txt", "dealabs1.txt") == true)
+            Console.WriteLine("Files are equal.");
     else
-    {
         Console.WriteLine("Files are not equal.");
-        StreamWriter sourceCode2Write = new StreamWriter("dealabs1.html", false);
-        sourceCode2Write.Write(texte);
-        sourceCode2Write.Close();
-    }
+
+    System.IO.File.Delete("dealabs1.txt");
+    System.IO.File.Copy("dealabs.txt", "dealabs1.txt");
 }
 
-/*
-    //Vérifie si le fichier à comparer existe
-    if (new FileInfo("dealabs1.html").Length == 0)
-    {
-        StreamWriter sourceCode2 = new StreamWriter("dealabs1.html");
-        sourceCode2.Write(texte);
-        sourceCode2.Close();
 
-    }
-    else
-    {
-        Console.WriteLine("NOT EMPTY !\n\n----------------------------------------------------------------------------------------");
-    }
-    StreamReader sourceCode2Read = new StreamReader("dealabs1.html");
-    StreamReader sourceCode1Read = new StreamReader("dealabs.html");
-    var compare2 = sourceCode2Read.ReadToEnd();
-    var compare1 = sourceCode1Read.ReadToEnd();
-    sourceCode1Read.Close();
-    sourceCode2Read.Close();
-
-
-
-    //Comparaison des ffichiers
-
-    fs1 = new FileStream("dealabs.html", FileMode.Open);
-    fs2 = new FileStream("dealabs1.html", FileMode.Open);
-
-    // Check the file sizes. If they are not the same, the files
-    // are not the same.
-    if (fs1.Length != fs2.Length)
-    {
-        // Close the file
-        fs1.Close();
-        fs2.Close();
-
-        // Return false to indicate files are different
-        Console.WriteLine("C'est pas les mêmes fichiers mon pote");
-    }
-
-    // Read and compare a byte from each file until either a
-    // non-matching set of bytes is found or until the end of
-    // file1 is reached.
-    do
-    {
-        // Read one byte from each file.
-        file1byte = fs1.ReadByte();
-        file2byte = fs2.ReadByte();
-    }
-    while ((file1byte == file2byte) && (file1byte != -1));
-
-    // Close the files.
-    fs1.Close();
-    fs2.Close();
-
-    // Return the success of the comparison. "file1byte" is
-    // equal to "file2byte" at this point only if the files are
-    // the same.
-    Console.WriteLine("Tout est ok ! C'est pareil");
-}
+//Récupère le code source de la page erreur de prix de dealabs et les écrit en txt
 
 
 /*TODO : 
- * filtrer sur dernier deal
- * si différents : nouveau deal en place
- * verifier si faux positifs
- * peut-être réduire la quantité de données a récup : moins sensibles aux modifs
- * hebeerger
+ * ajouter au bot twitter
+ * faire fonctionner en 24/7
 */
